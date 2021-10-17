b3b781cf70cf6512d79ea11b3697bb02d3785505
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-10-17 10:25:37 +0100

    Add PointLight, PointLightHelper & AmbientLight

--- main.js
+++ main.js
@@ -18,11 +18,17 @@ camera.position.setZ(30)
 
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial({ color: 0xff6347, wireframe: true})
-
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347})
 const torus = new THREE.Mesh(geometry, material)
-
 scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+const pointLightHelper = new THREE.PointLightHelper(pointLight)
+const ambientLight = new THREE.AmbientLight(0xffffff)
+pointLight.position.set(0,0,0)
+scene.add(pointLight)
+scene.add(pointLightHelper)
+scene.add(ambientLight)
 
 function animate(){
     requestAnimationFrame(animate)
